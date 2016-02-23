901f58b0f13c844390e433b4d78a063262313d11
Klara <klarakollerstrom@gmail.com>
2016-02-23 15:02:39 +0100

    deleted old file
    deleted old file

--- wp-content/themes/qlokare2/functions_general.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	//require_once plugin_dir_path( __FILE__ ) . 'functions/functions-custom-register.php';
-	require_once plugin_dir_path( __FILE__ ) . 'functions-custom-post-types.php';
-	require_once plugin_dir_path( __FILE__ ) . 'functions_login.php';
-	//require_once plugin_dir_path( __FILE__ ) . 'functions/functions-custom-taxonomies.php';
-	//require_once plugin_dir_path( __FILE__ ) . 'functions/functions-widgets-and-sidebars.php';
